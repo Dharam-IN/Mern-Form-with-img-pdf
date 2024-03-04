5f5edac66581196f5c5367e9f9e2d40d88aa04d5
dharam-in <dharamrajyadav6312@gmail.com>
2024-03-04 23:20:57 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+  console.log(import.meta.env.VITE_MY_APP)
+
+  return (
+    <>
+      <h1>Blog App using appwrite</h1>
+    </>
+  )
+}
+
+export default App
